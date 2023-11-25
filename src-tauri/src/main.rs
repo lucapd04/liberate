0ba0e2782b615043acc436fff7f445c967036e41
Inuayasha <themagician948@gmail.com>
2023-11-25 13:59:15 -0500

    added system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
+use tauri::SystemTray;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri:Manager;
+
 
 fn main() {
+  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+  .add_item(quit)
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(hide);
+
+  let system_tray = SystemTray::new().with_menu(tray_menu);
+
   tauri::Builder::default()
+    .system_tray(system_tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
