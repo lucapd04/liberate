9ca834b784dc33263a16743b95431f38ffc64d50
Everett <62971030+3verett@users.noreply.github.com>
2023-11-26 02:05:17 -0500

    Adds values to buttons in main menu; semi completes textbox.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,8 @@ import { invoke } from '@tauri-apps/api/tauri'
 import { Button } from '@/components/ui/button'
 import React, { useState } from 'react';
 import ChooseApp from '../components/ChooseApp';
-
+import { userEnviroBtn } from './functions';
+import { continueBtn } from './functions';
 
 
 
@@ -29,17 +30,17 @@ return (
         <div className="absolute w-[266px] top-0 left-[79px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#fffdfd] text-[20px] tracking-[0] leading-[normal] whitespace-nowrap">
           WHERE YOU’RE LOCATED
         </div>
-        <button className="absolute w-[422px] h-[67px] top-[52px] left-0 bg-[#d9d9d93d] rounded-[10px]">
+        <button onClick={() => userEnviroBtn(1)} className="absolute w-[422px] h-[67px] top-[52px] left-0 bg-[#d9d9d93d] rounded-[10px]">
         <div className="absolute w-[87px] top-[24px] left-[167px] [font-family:'Inter-Medium',Helvetica] font-medium text-white text-[20px] tracking-[4.00px] leading-[normal]">
           URBAN
         </div>
       </button>
-      <button className="absolute w-[422px] h-[67px] top-[249px] left-0 bg-[#d9d9d93d] rounded-[10px]">
+      <button onClick={() => userEnviroBtn(3)} className="absolute w-[422px] h-[67px] top-[249px] left-0 bg-[#d9d9d93d] rounded-[10px]">
         <div className="absolute w-[84px] top-[23px] left-[170px] [font-family:'Inter-Medium',Helvetica] font-medium text-white text-[20px] tracking-[4.00px] leading-[normal]">
           RURAL
         </div>
       </button>
-      <button className="absolute w-[422px] h-[68px] top-[150px] left-0 bg-[#d9d9d93d] rounded-[10px]">
+      <button onClick={() => userEnviroBtn(2)} className="absolute w-[422px] h-[68px] top-[150px] left-0 bg-[#d9d9d93d] rounded-[10px]">
         <div className="absolute w-[161px] top-[23px] left-[147px] [font-family:'Inter-Medium',Helvetica] font-medium text-white text-[20px] tracking-[4.00px] leading-[normal]">
           SUBURBAN
         </div>
@@ -53,10 +54,9 @@ return (
           className="absolute w-[257px] top-[18px] left-[31px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-[#ffffff7d] text-[20px] tracking-[0] leading-[normal] bg-transparent border-none outline-none"
           type="text"
           placeholder="I enjoy..."
+          name="txtBox"
         />
       </div>
-      
-
       <div className="absolute w-[188px] h-[219px] top-[557px] left-[56px]">
         <button className="relative w-[186px] h-[219px] bg-[#d9d9d93d] rounded-[10px]">
           <div className="w-[186px] h-[219px] bg-[#d9d9d93d] rounded-[10px] absolute top-0 left-0" />
@@ -92,7 +92,8 @@ return (
       </p>
 
       <div className="absolute w-[504px] h-[62px] top-[693px] left-[644px]">
-        <button className="relative w-[502px] h-[62px] bg-[#d9d9d93d] rounded-[100px]">
+        /** Name of text box is "txtBox" */
+        <button onClick={() => continueBtn()} className="relative w-[502px] h-[62px] bg-[#d9d9d93d] rounded-[100px]">
           <div className="absolute w-[180px] top-[19px] left-[161px] [font-family:'Inter-SemiBold',Helvetica] font-semibold text-white text-[25px] tracking-[5.00px] leading-[normal] whitespace-nowrap">
             CONTINUE
           </div>
@@ -102,8 +103,6 @@ return (
     </div>
   </div>
 );
-
-    
 
 <div>
   <ChooseApp/>
